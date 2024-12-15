030f025d1a046c0f29f3911e7f254e9be2b78cde
Charles Caleb <charlesofficial9@gmail.com>
2024-12-15 16:01:08 +0100

    update

--- src/app.js
+++ src/app.js
@@ -29,8 +29,10 @@ app.use(helmet());
 // Rate Limiting
 const limiter = rateLimit({
   max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many requests from this IP, please try again in an hour!",
+  // windowMs: 60 * 60 * 1000,
+  // message: "Too many requests from this IP, please try again in an hour!",
+  windowMs: 2 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in two mins!",
 });
 app.use("/api", limiter);
 
