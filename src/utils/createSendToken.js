9fcf097c625e23bec478b35e43dbee57a96261f8
Charles Caleb <charlesofficial9@gmail.com>
2024-12-25 21:59:09 +0100

    update

--- src/utils/createSendToken.js
+++ src/utils/createSendToken.js
@@ -5,14 +5,14 @@ const handleResponse = require("./handleResponse");
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user.id);
 
-  const cookieOptions = {
+  res.cookie("jwt", token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-  };
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+  });
 
-  res.cookie("jwt", token, cookieOptions);
   user.password = undefined;
 
   // console.log("Token:", token);
