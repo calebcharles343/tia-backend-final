480f022aabfa5eae239bab1514f4fa695fad4cac
Charles Caleb <charlesofficial9@gmail.com>
2024-12-26 10:23:58 +0100

    update

--- src/utils/createSendToken.js
+++ src/utils/createSendToken.js
@@ -6,12 +6,14 @@ const signToken = require("../utils/signToken");
 const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user.id);
 
+  const secure = req.secure || req.headers["x-forwarded-proto"] === "https";
+
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+    secure: secure,
     sameSite: "strict", // Ensuring the cookie is only sent in requests to the same site
   };
 
