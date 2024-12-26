299638381dca2c21f6081c841cad977475e8d8c5
Charles Caleb <charlesofficial9@gmail.com>
2024-12-26 10:04:20 +0100

    update

--- src/app copy.js
+++ src/app copy.js
@@ -15,18 +15,13 @@ const reviewRouter = require("./routes/reviewRoutes.js");
 const a3BucketRouter = require("./routes/a3BucketRoutes.js");
 const multer = require("multer");
 const { memoryStorage } = multer;
+// const paymentRoutes = require("./routes/paymentRoutes");
 
 dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://your-client-domain", // Allow your client's domain
-    credentials: true, // Enable cookies
-  })
-);
-
+app.use(cors());
 app.use(express.json({ limit: "10kb" }));
 
 // Security Headers
@@ -35,12 +30,15 @@ app.use(helmet());
 // Rate Limiting
 const limiter = rateLimit({
   max: 100,
+  // windowMs: 60 * 60 * 1000,
+  // message: "Too many requests from this IP, please try again in an hour!",
   windowMs: 2 * 60 * 1000,
   message: "Too many requests from this IP, please try again in two mins!",
 });
 app.use("/api", limiter);
 
 // Swagger Documentation
+// Apply specific CORS handling for Swagger UI if needed
 app.use(
   "/e-commerce/api-docs",
   swaggerUi.serve,
@@ -57,7 +55,7 @@ app.use("/api/v1/e-commerce/products", productRouter);
 app.use("/api/v1/e-commerce/orders", orderRouter);
 app.use("/api/v1/e-commerce/reviews", reviewRouter);
 app.use("/api/v1/e-commerce/images", a3BucketRouter);
-
+// app.use("/api/v1/e-commerce/payment", paymentRoutes);
 // Error Handling
 app.use(errorHandler);
 
--- src/utils/createSendToken.js
+++ src/utils/createSendToken.js
@@ -1,22 +1,18 @@
 "use strict";
 
 const handleResponse = require("./handleResponse");
-
 const signToken = require("../utils/signToken");
 
-/*////////////////////////////////////// */
-/*////////////////////////////////////// */
-
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user.id);
 
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-
     httpOnly: true,
-    secure: true,
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+    sameSite: "strict", // Ensuring the cookie is only sent in requests to the same site
   };
 
   user.password = undefined;
