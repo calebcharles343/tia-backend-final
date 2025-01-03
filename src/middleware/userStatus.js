bfbf8eb801d6dbafb4b43739a55cf34a6922accd
Charles Caleb <charlesofficial9@gmail.com>
2025-01-03 17:49:06 +0100

    update

--- src/middleware/userStatus.js
+++ src/middleware/userStatus.js
@@ -2,7 +2,7 @@ const AppError = require("../utils/appError");
 
 const userStatus = (...status) => {
   return (req, res, next) => {
-    if (!status.includes(req.body.status)) {
+    if (!status.includes(req.body.role)) {
       return next(
         new AppError("User role options are only Admin or User", 403)
       );
